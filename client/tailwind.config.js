251ad505eb140e3583aa3f9cd919d167494b68b0
Jennifer H Thompson <writejthompson@gmail.com>
2025-04-07 18:56:06 -0600

    fixing tailwind config real quick

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,15 +6,6 @@ export default {
       backgroundImage: {
         'money-bg': "url('/assets/MoneyMagicBG.png')",
       },
-      keyframes: {
-        twinkle: {
-          '0%, 100%': { opacity: '1' },
-          '50%': { opacity: '0.2' },
-        },
-      },
-      animation: {
-        twinkle: 'twinkle 1.5s ease-in-out infinite',
-      },
     },
   },
   plugins: [],
