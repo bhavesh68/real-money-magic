98fe6c8b73f2f47783726fd6ccb0a687b2b52db5
Amarrero0215 <alexmarrero0215@gmail.com>
2025-04-09 01:16:54 -0400

    Update PublicRoute.tsx
    needs to be updated for working frontend

--- client/src/components/PublicRoute.tsx
+++ client/src/components/PublicRoute.tsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom';
 const PublicRoute = ({ children }: { children: ReactNode }) => {
   const token = localStorage.getItem('token');
   console.log('[🌐 PublicRoute] token exists:', !!token);
-  return token ? <Navigate to="/public" /> : children;
+  return token ? <Navigate to="/profile" /> : children;
 };
 
 export default PublicRoute;
